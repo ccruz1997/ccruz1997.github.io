d29e7e6d21b1764132a08a5ad802bea72439a4fa
ccruz1997 <cpcruz97@gmail.com>
2022-11-28 20:57:42 -0500

    bar graph by agency functional

--- script.js
+++ script.js
@@ -8,22 +8,89 @@ async function getData() {
   } catch (err) {
     console.log('Request failed', err);
   }
-}
+};
 
+/*
 function uniquePayees(data){
     reply = data.map((item) => item.payee_name).filter((value, index, self) => self.indexOf(value) === index);
     return reply;
 } 
 
+function dataByPayee(data,payees) {
+    let newData = data;
+    let newPayees = payees;
+    let dataOnPayee = new Array(payees.length);
+    
+    for (i = 0; i < dataOnPayee.length; i++) {
+        dataOnPayee[i] = new Array();
+    }
+
+    console.log(dataOnPayee);
+
+    newData.forEach(element => {
+       dataOnPayee[newPayees.indexOf(element.payee_name)].push(element.amount)
+    });
+
+    console.log(dataOnPayee)
+    return dataOnPayee;
+
+}
+*/
+function initChart(chart, object){
+  const labels = Object.keys(object);
+  const info = Object.keys(object).map((item) => object[item].length);
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: info
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };  
+
+  return new Chart(
+    chart,
+    config
+  );
+};
+
+function shapeDataForChart(array) {
+    return array.reduce((collection,item) => {
+        if (!collection[item.agency]){
+            collection[item.agency] = [item];
+        } else {
+            collection[item.agency].push(item);
+        }
+        return collection;
+    },{});
+}
+
+
+
 async function MainEvent() {
-  let response = await getData();
-  response = response.filter((item) => Boolean(item.agency === 'POLICE')).filter((item) => item.amount > 0);
-  console.log(response);
+  const chartTarget = document.querySelector('#myChart');
+  const chartData = await getData();
+  const shapedData = shapeDataForChart(chartData);
 
+  initChart(chartTarget, shapedData);
+  console.log(shapedData);
+/*
   payees = uniquePayees(response);
-  console.log(payees);
+  amountByPayee = dataByPayee(response,payees)
 
-  console.log(byPayee(response,'COMCAST'));
+  labels = payees;
+  datasets = amountByPayee;
+
+  console.log(labels);
+  console.log(datasets);
+  */
 }
 
 document.addEventListener('DOMContentLoaded', async () => MainEvent());
